e1cda68b54eccf9717a86da9c2188824a1ab6868
Everett Eng <93930051+EverettEng@users.noreply.github.com>
2021-12-21 21:54:02 +0000

    a

--- part4.py
+++ part4.py
@@ -40,3 +40,17 @@ fizz
 """
 
 # write your code here 
+
+number = int(input("Enter a number: "))
+
+
+for i in range(1, number+1):
+  if((i%5==0) & (i%3==0)):
+    print("fizzbuzz")
+  elif(i%3 == 0):
+    print("fizz")
+  elif(i%5== 0):
+    print("buzz")
+  else:
+    print(i)      
+
